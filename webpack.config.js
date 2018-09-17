0dfafe5dea2b223eab19efa2ad680095563fdada
Tzobree <tzobree@gmail.com>
2018-09-17 12:36:59 +0200

    Completed header

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+	entry: "./app/assets/scripts/App.js",
+	output: {
+		path: __dirname + "/app/temp/scripts",
+		filename: "App.js"
+	},
+	module: {
+		rules: [
+			{
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015']
+				},
+				test: /\.js$/,
+				exclude: /node_modules/
+			}
+		]
+	}
+}
