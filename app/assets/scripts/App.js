625f36c0600bf322e98629ecfa308568cbb83621
Tzobree <tzobree@gmail.com>
2018-09-17 14:36:37 +0200

    Fade in completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
+import $ from 'jquery';
+import RevealOnScroll from './modules/RevealOnScroll';
+
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
